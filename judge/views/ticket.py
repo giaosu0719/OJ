bc0ec034abd9ed50afb2b6a0f43b99c944050183
Tran Quoc Viet <vtqktvn123@gmail.com>
2026-07-10 19:49:41 +0700

    hihi

--- judge/views/ticket.py
+++ judge/views/ticket.py
@@ -212,7 +212,10 @@ class TicketView(TitleMixin, TicketMixin, SingleObjectFormView):
         context['ticket_messages'] = self.object.messages.select_related('user__user')
         context['assignees'] = self.object.assignees.select_related('user', 'display_badge')
         if self.request.profile != self.object.user:
-            context['autofill_replies'] = json.dumps(getattr(settings, 'TICKET_AUTOFILL_REPLIES', []))
+            context['autofill_replies'] = json.dumps(
+                getattr(settings, 'TICKET_AUTOFILL_REPLIES', []),
+                ensure_ascii=False,
+            )
         return context
 
 
